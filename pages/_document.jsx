567015c0a7bd781a4e9242d8faaf14d5c8240299
Luiz Henrique <lpansarini@gmail.com>
2021-01-15 13:19:25 -0300

    feat(material-table): create material-table and add some data

---
+++ pages/_document.jsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,66 @@
 import Layout from '../components/Layout'
 
+import MaterialTable from 'material-table'
+
 const Home = () => {
+  const [columns, setColumns] = React.useState([
+    { title: 'Nome', field: 'name' },
+    {
+      title: 'Sobrenome',
+      field: 'lastname'
+    },
+    {
+      title: 'Sexo',
+      field: 'gender',
+      lookup: { 0: 'Masculino', 1: 'Feminino', 2: 'Não declarado' }
+    }
+  ])
+
+  const [data, setData] = React.useState([
+    { name: 'Mehmet', lastname: 'Baran', gender: 2 },
+    { name: 'Zerya Betül', lastname: 'Baran', gender: 0 }
+  ])
+
   return (
     <Layout title="Home - Nextale Test">
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
-        </h1>
+      <div style={{ maxWidth: '100%' }}>
+        <MaterialTable
+          title="Person Table"
+          columns={columns}
+          data={data}
+          editable={{
+            onRowAdd: newData =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  setData([...data, newData])
+
+                  resolve()
+                }, 1000)
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  const dataUpdate = [...data]
+                  const index = oldData.tableData.id
+                  dataUpdate[index] = newData
+                  setData([...dataUpdate])
+
+                  resolve()
+                }, 1000)
+              }),
+            onRowDelete: oldData =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  const dataDelete = [...data]
+                  const index = oldData.tableData.id
+                  dataDelete.splice(index, 1)
+                  setData([...dataDelete])
+
+                  resolve()
+                }, 1000)
+              })
+          }}
+        />
       </div>
     </Layout>
   )
